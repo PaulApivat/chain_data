9c5c4fb672bf1a84e053927adf7710ca70c4cd35
paulapivat <paul.apivat@gmail.com>
2021-04-01 16:06:39 +0700

    add unique receivers

--- dune/grt.sql
+++ dune/grt.sql
@@ -1,7 +1,7 @@
 /* RESOURCE */
 https://etherscan.io/token/0xc944e90c64b2c07662a292be6244bdf05cda44a7
 
-/* Query Contract Address / Token Symbol */
+/* Query Contract Address / Token Symbol - Time Series */
 SELECT * FROM erc20."tokens"
 WHERE symbol='GRT'
 LIMIT 10
@@ -10,7 +10,7 @@ SELECT * FROM erc20."tokens"
 WHERE contract_address='\xc944e90c64b2c07662a292be6244bdf05cda44a7'
 LIMIT 10
 
-/* Token TRANSFER AMOUNT GRT */
+/* Token TRANSFER AMOUNT GRT - Time Series */
 SELECT 
 SUM("value"/10^18) AS "value",
 DATE_TRUNC('day', evt_block_time) AS dt
@@ -18,5 +18,20 @@ FROM erc20."ERC20_evt_Transfer"
 WHERE contract_address='\xc944e90c64b2c07662a292be6244bdf05cda44a7'
 GROUP BY dt
 
+/* Token TRANSFER COUNT GRT - Time Series */
+SELECT 
+COUNT(contract_address) AS contract_address_count,
+DATE_TRUNC('day', evt_block_time) AS dt
+FROM erc20."ERC20_evt_Transfer"
+WHERE contract_address='\xc944e90c64b2c07662a292be6244bdf05cda44a7'
+GROUP BY dt
+
+/* Unique Receivers GRT Token - Time Series */
+SELECT 
+COUNT(DISTINCT("to")) AS unique_receivers,
+DATE_TRUNC('day', evt_block_time) AS dt
+FROM erc20."ERC20_evt_Transfer"
+WHERE contract_address='\xc944e90c64b2c07662a292be6244bdf05cda44a7'
+GROUP BY dt
 
 
