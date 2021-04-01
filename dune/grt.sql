ac09d4aaed8dc0d7298bc9021ab3d9be9e09c615
paulapivat <paul.apivat@gmail.com>
2021-04-01 20:19:59 +0700

    update grt.sql

--- dune/grt.sql
+++ dune/grt.sql
@@ -41,3 +41,60 @@ DATE_TRUNC('day', evt_block_time) AS dt
 FROM erc20."ERC20_evt_Transfer"
 WHERE contract_address='\xc944e90c64b2c07662a292be6244bdf05cda44a7'
 GROUP BY dt
+
+/* Unique Owner, Total Value Held */
+SELECT 
+COUNT(DISTINCT(owner)) AS unique_owner, 
+SUM("value") AS sum_value, 
+DATE_TRUNC('day', evt_block_time) AS dt
+FROM erc20."ERC20_evt_Approval"
+WHERE contract_address='\xc944e90c64b2c07662a292be6244bdf05cda44a7'
+GROUP BY dt
+
+/* Top 100 Unique Owner by Total Value (GRT Token) */
+SELECT 
+DISTINCT(owner) as distinct_owner,
+SUM("value"/10^18) AS sum_value
+FROM erc20."ERC20_evt_Approval"
+WHERE contract_address='\xc944e90c64b2c07662a292be6244bdf05cda44a7'
+GROUP BY distinct_owner
+ORDER BY sum_value DESC
+LIMIT 100
+
+
+/* Top 100 Owner & Spender by Total Value (GRT Token) */
+SELECT 
+owner,
+SUM("value"/10^18) AS sum_value
+FROM erc20."ERC20_evt_Approval"
+WHERE contract_address='\xc944e90c64b2c07662a292be6244bdf05cda44a7'
+GROUP BY owner
+ORDER BY sum_value DESC
+LIMIT 100
+
+
+SELECT 
+spender,
+SUM("value"/10^18) AS sum_value
+FROM erc20."ERC20_evt_Approval"
+WHERE contract_address='\xc944e90c64b2c07662a292be6244bdf05cda44a7'
+GROUP BY spender
+ORDER BY sum_value DESC
+LIMIT 100
+
+/* If unsure DISTINCT(owner) makes a different do a comparison count */
+/* Makes a huge difference */
+
+/* Number of Token Owners 120555 */
+SELECT 
+COUNT(owner)
+FROM erc20."ERC20_evt_Approval"
+WHERE contract_address='\xc944e90c64b2c07662a292be6244bdf05cda44a7'
+LIMIT 10
+
+/* Number of Unique Token Owners 25340 */
+SELECT 
+COUNT(DISTINCT(owner))
+FROM erc20."ERC20_evt_Approval"
+WHERE contract_address='\xc944e90c64b2c07662a292be6244bdf05cda44a7'
+LIMIT 10
