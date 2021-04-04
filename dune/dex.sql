3121aed13c1e87ee9018118e1d99542da5ed9f08
paulapivat <paul.apivat@gmail.com>
2021-04-04 15:30:59 +0700

    add all DEX ordered by 7-day volume

--- dune/dex.sql
+++ dune/dex.sql
@@ -1,6 +1,27 @@
 /* DEX 24 Hours Volume */
+SELECT
+    SUM(usd_amount)/1e9 AS billion_volume
+FROM dex."trades"
+WHERE block_time > now() - interval '24 hours'
+AND category = 'DEX'
+
+
+/* note: two categories - DEX & Aggregator */
+SELECT
+    DISTINCT(category) AS distinct_category
+FROM dex."trades"
+LIMIT 50
+
+
 
 /* DEX 7 Day Volume */
+/* note: time interval can change, 24 hours, 7 days, 1 month */
+
+SELECT
+    SUM(usd_amount)/1e9 AS billion_volume
+FROM dex."trades"
+WHERE block_time > now() - interval '7 days'
+AND category = 'DEX'
 
 /* Aggregators 7 days Volume */
 
@@ -10,7 +31,24 @@
 
 /* Aggregator share of DEX volume */
 
-/* Ranked DEX by Volume */
+/* Ranked DEX by Volume  */
+
+/* Exclude Uniswap V2 - "exchange_contract_address" != '\xf9c1fA7d41bf44ADe1dd08D37CC68f67Ae75bF92' */
+
+/* Ranked DEX by Volume (7-days)  */
+/* NOTE: number formatting inside Dune UI */
+
+SELECT
+    project,
+    SUM(usd_amount) AS "7-Day Volume"
+FROM dex."trades"
+WHERE block_time > now() - interval '7 days'
+AND category = 'DEX'
+AND "exchange_contract_address" != '\xf9c1fA7d41bf44ADe1dd08D37CC68f67Ae75bF92' 
+GROUP BY project
+ORDER BY "7-Day Volume" DESC
+LIMIT 20
+
 
 /* Market Share DEX by Volume */
 
