4fbadcd5a2ec849d8bd363ba46a70f08502cc135
paulapivat <paul.apivat@gmail.com>
2021-04-04 14:17:55 +0700

    update readme and add dex.sql

---
+++ dune/dex.sql
@@ -0,0 +1,42 @@
+/* DEX 24 Hours Volume */
+
+/* DEX 7 Day Volume */
+
+/* Aggregators 7 days Volume */
+
+/* DEX Trailing 24 Hours Growth */
+
+/* DEX Trailing 7 Days Growth */
+
+/* Aggregator share of DEX volume */
+
+/* Ranked DEX by Volume */
+
+/* Market Share DEX by Volume */
+
+/* Aggregator share of DEX volume */
+
+/* Daily DEX Volume */
+
+/* Number of traders last 7 Days */
+
+/* Ranked Aggregator by Volume */
+
+/*** WEEKLY  ***/
+
+/* Weekly DEX Volume */
+
+/* Market Share Weekly DEX Volume */
+
+/*** MONTHLY  ***/
+
+/* Monthly DEX Volume By Project */
+
+/* Monthly DEX Volume grouped by year */
+
+/* 12 Months DEX volume */
+
+/* 30 days DEX volume */
+
+/* Counter: Number of DEX Traders */
+
