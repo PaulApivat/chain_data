61372f92d57f7150ae8e2bf7134d1dae221c4185
paulapivat <paul.apivat@gmail.com>
2021-04-05 20:16:00 +0700

    add Known Origin and Rarible token

--- dune/nft.sql
+++ dune/nft.sql
@@ -37,3 +37,22 @@ WHERE "from" = '\x0000000000000000000000000000000000000000'
 AND EXTRACT (year from evt_block_time) >= 2018
 GROUP BY month_created
 LIMIT 50
+
+/* Known Origin: NFT Artist Onboarding by Month */ 
+SELECT 
+   COUNT(DISTINCT("_creator")) AS artist_addr,
+   DATE_TRUNC('month', evt_block_time) AS month_created
+FROM knownorigin."SelfServiceEditionCurationV4_evt_SelfServiceEditionCreated"
+WHERE EXTRACT (year from evt_block_time) >= 2018
+GROUP BY month_created
+LIMIT 50
+
+/* Rarible v1: NFT Artist Onboarding by Month */ 
+SELECT 
+   COUNT(DISTINCT("_to")) AS artist_addr,
+   DATE_TRUNC('month', evt_block_time) AS month_created
+FROM rarible_v1."RaribleToken_v1_evt_TransferSingle" 
+WHERE "_from" = '\x0000000000000000000000000000000000000000'
+AND EXTRACT (year from evt_block_time) >= 2018
+GROUP BY month_created
+LIMIT 50
