b231dc428eaec850b875eb7f5107aba3f871159e
paulapivat <paul.apivat@gmail.com>
2021-05-08 15:43:33 +0700

    add bank token trade pair pie chart

--- dune/bank_token.sql
+++ dune/bank_token.sql
@@ -122,3 +122,63 @@ SELECT
 FROM temp_table
 GROUP BY 1,2
 ORDER BY dt DESC
+
+/* Total BANK Trade Volume */
+WITH temp_table AS (
+    SELECT
+        block_time,
+        token_a_symbol,
+        token_b_symbol,
+        token_a_amount,
+        token_b_amount,
+        usd_amount,
+        token_b_address,
+        token_a_address,
+        project,
+        version
+    FROM dex."trades"
+WHERE token_a_address = '\x2d94aa3e47d9d5024503ca8491fce9a2fb4da198' OR token_b_address = '\x2d94aa3e47d9d5024503ca8491fce9a2fb4da198'
+), temp_table2 AS (
+SELECT
+    DATE_TRUNC('day', block_time) as dt,
+    CASE WHEN token_a_address = '\x2d94aa3e47d9d5024503ca8491fce9a2fb4da198' THEN token_b_symbol ELSE token_a_symbol END AS token_traded_against,
+    SUM(usd_amount) AS sum_usd
+FROM temp_table
+GROUP BY 1,2
+ORDER BY dt DESC
+)
+SELECT
+    SUM(sum_usd) AS total_volume
+FROM temp_table2
+
+/* Total BANK Trade Vol. BY Token PAIR */
+/* Pie Chart input */
+WITH temp_table AS (
+    SELECT
+        block_time,
+        token_a_symbol,
+        token_b_symbol,
+        token_a_amount,
+        token_b_amount,
+        usd_amount,
+        token_b_address,
+        token_a_address,
+        project,
+        version
+    FROM dex."trades"
+WHERE token_a_address = '\x2d94aa3e47d9d5024503ca8491fce9a2fb4da198' OR token_b_address = '\x2d94aa3e47d9d5024503ca8491fce9a2fb4da198'
+), temp_table2 AS (
+SELECT
+    DATE_TRUNC('day', block_time) as dt,
+    CASE WHEN token_a_address = '\x2d94aa3e47d9d5024503ca8491fce9a2fb4da198' THEN token_b_symbol ELSE token_a_symbol END AS token_traded_against,
+    SUM(usd_amount) AS sum_usd
+FROM temp_table
+GROUP BY 1,2
+ORDER BY dt DESC
+)
+SELECT
+    SUM(sum_usd) AS total_volume,
+    token_traded_against
+FROM temp_table2
+GROUP by 2
+ORDER by token_traded_against DESC
