a6d7f2701ea39ac25cb4da2852645548f45fdd3e
paulapivat <paul.apivat@gmail.com>
2021-05-20 10:51:50 +0700

    pivot longer in postgresql

--- dune/bank_holders.sql
+++ dune/bank_holders.sql
@@ -269,3 +269,60 @@ SELECT
     percentage
 FROM temp_table3
 WHERE balance >= 1
+
+/* Bank Holder Categories */
+/* Trying to Pivot Longer */
+/* But need to insert another column */
+
+WITH temp_table AS (
+SELECT 
+    evt_block_time,
+    tr."from" AS address,
+    -tr.value AS amount,
+    contract_address
+FROM erc20."ERC20_evt_Transfer" tr
+WHERE contract_address = '\x2d94aa3e47d9d5024503ca8491fce9a2fb4da198'
+
+UNION ALL
+
+SELECT
+    evt_block_time,
+    tr."to" AS address,
+    tr.value AS amount,
+    contract_address
+FROM erc20."ERC20_evt_Transfer" tr
+WHERE contract_address = '\x2d94aa3e47d9d5024503ca8491fce9a2fb4da198'
+), temp_table2 AS (
+SELECT
+    address,
+    SUM(amount/10^18) AS balance
+FROM temp_table tr
+LEFT JOIN erc20.tokens tok ON tr.contract_address = tok.contract_address
+GROUP BY 1
+ORDER BY 2 DESC
+), temp_table3 AS (
+SELECT
+    COUNT(CASE WHEN balance >= 100000000 THEN 1 END) AS hundred_millions,
+    COUNT(CASE WHEN balance >= 1000000 AND balance < 100000000 THEN 1 END) AS millions,
+    COUNT(CASE WHEN balance >= 500000 AND balance < 1000000 THEN 1 END) AS five_hundred_k,
+    COUNT(CASE WHEN balance >= 150000 AND balance < 500000 THEN 1 END) AS one_fifty_k,
+    COUNT(CASE WHEN balance >= 35000 AND balance < 150000 THEN 1 END) AS thirty_five_k,
+    COUNT(CASE WHEN balance >= 10000 AND balance < 35000 THEN 1 END) AS ten_k,
+    COUNT(CASE WHEN balance >= 1 AND balance < 10000 THEN 1 END) AS less_ten_k
+FROM temp_table2
+WHERE balance >= 1
+)
+SELECT hundred_millions FROM temp_table3
+UNION ALL
+SELECT millions FROM temp_table3
+UNION ALL
+SELECT five_hundred_k FROM temp_table3
+UNION ALL
+SELECT one_fifty_k FROM temp_table3
+UNION ALL
+SELECT thirty_five_k FROM temp_table3
+UNION ALL
+SELECT ten_k FROM temp_table3
+UNION ALL
+SELECT less_ten_k FROM temp_table3
+
